a1f531fab8d6fe403eb4f8c29a87d8e24c858726
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2025-08-04 05:03:53 +0900

    nothing

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -28,7 +28,7 @@ const ScanScreen = () => {
   const [showFallback, setShowFallback] = useState(false);
   const [fallbackFact, setFallbackFact] = useState('');
 
-  const SERVER_URL = 'https://waste-classifier-modal-7.onrender.com';
+  const SERVER_URL = 'https://waste-classifier-modal-2.onrender.com/predict';
   const SERVER_URL_1 = 'https://waste-classifier-modal-6.onrender.com';
 
   const ecoFacts = [
